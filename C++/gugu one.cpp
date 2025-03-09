c229298479ce9c35aef453db6f798111b014944e
chopssal <koalasunghee@gmail.com>
2025-03-09 20:32:54 +0900

    Added a gugu two file

--- C++/gugu one.cpp
+++ C++/gugu one.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
+// #hash mark 
+// stdio.h: standard input output
+
 int main(){
     int i = 1;
     int dan = 3;
-    printf("절차적 프로그래밍\n");
 
     loop:
         printf("%2d x %2d = %2d\n, dan, i, dan*i");
