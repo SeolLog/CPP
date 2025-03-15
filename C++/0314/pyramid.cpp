623046d25c844335d400790d88f73a85422f89bb
chopssal <koalasunghee@gmail.com>
2025-03-15 14:44:28 +0900

    add: 0315

--- C++/0314/pyramid.cpp
+++ C++/0314/pyramid.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-    int height;
-    scanf()
-
+    for(int i = 5; i >= 1; i--){
+        for(int z = 5; z>= i; z--){
+            cout << z << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
+
+// 5
+// 4
